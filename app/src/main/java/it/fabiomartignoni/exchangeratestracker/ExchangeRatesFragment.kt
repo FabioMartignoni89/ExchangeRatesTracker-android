a30d05c6f65f8239dad24781188de512da854fdf
Fabio Martignoni <fabiomartignoni89@gmail.com>
2020-04-01 17:44:31 +0200

    Project setup

---
+++ app/src/main/java/it/fabiomartignoni/exchangeratestracker/ExchangeRatesFragment.kt
@@ -0,0 +1,27 @@
+package it.fabiomartignoni.exchangeratestracker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class ExchangeRatesFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_exchange_rates, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_add_exchange_rates).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+}
