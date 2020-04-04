c46805f12344c5bb26b0be3a890dfe163d68deb6
Fabio Martignoni <fabiomartignoni89@gmail.com>
2020-04-04 16:46:52 +0200

    Exchange rates list implementation

---
+++ app/src/main/java/it/fabiomartignoni/exchangeratestracker/view/ExchangeRatesAdapter.kt
@@ -0,0 +1,58 @@
+package it.fabiomartignoni.exchangeratestracker.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import it.fabiomartignoni.exchangeratestracker.R
+import it.fabiomartignoni.exchangeratestracker.viewmodel.ExchangeRateDisplayModel
+import kotlinx.android.synthetic.main.exchange_rate_row.view.*
+
+class ExchangeRatesAdapter: RecyclerView.Adapter<ExchangeRatesAdapter.ExchangeRateHolder>()  {
+
+    private var exchangeRates: List<ExchangeRateDisplayModel> = ArrayList()
+
+    fun updateExchangeRates(exchangeRates: List<ExchangeRateDisplayModel>) {
+        this.exchangeRates = exchangeRates
+        this.notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return exchangeRates.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExchangeRateHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.exchange_rate_row, parent, false)
+        return ExchangeRateHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ExchangeRateHolder, position: Int) {
+        holder.bind(exchangeRates[position])
+    }
+
+    class ExchangeRateHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+
+        private var view: View = v
+        private var exchangeRate: ExchangeRateDisplayModel? = null
+
+        init {
+            v.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View) {
+
+        }
+
+        fun bind(exchangeRate: ExchangeRateDisplayModel) {
+            this.exchangeRate = exchangeRate
+            view.currencyPairTextView.text = exchangeRate.currencyPair
+            view.exchangeRateTextView.text = exchangeRate.exchangeRate
+        }
+
+    }
+
+
+}
+
--- app/src/main/java/it/fabiomartignoni/exchangeratestracker/view/ExchangeRatesFragment.kt
+++ app/src/main/java/it/fabiomartignoni/exchangeratestracker/view/ExchangeRatesFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import it.fabiomartignoni.exchangeratestracker.R
 import it.fabiomartignoni.exchangeratestracker.viewmodel.ExchangeRatesViewModel
 import it.fabiomartignoni.exchangeratestracker.viewmodel.ExchangeRatesViewModelFactory
@@ -21,38 +22,44 @@ class ExchangeRatesFragment : Fragment(), NewExchangeRateFragment.NewExchangeRat
 
     var viewModel: ExchangeRatesViewModel? = null
 
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var exchangeRatesAdapter: ExchangeRatesAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.fragment_exchange_rates, container, false)
 
-        if (activity != null) {
-            viewModel = ViewModelProvider(this, ExchangeRatesViewModelFactory(activity!!.baseContext))
-                .get(ExchangeRatesViewModel::class.java)
-            setupBindings()
-        }
+        viewModel = ViewModelProvider(this, ExchangeRatesViewModelFactory(requireActivity().baseContext))
+            .get(ExchangeRatesViewModel::class.java)
 
         return view
     }
 
-    private fun setupBindings() {
-        viewModel?.exchangeRates?.observe(viewLifecycleOwner, Observer { exchangeRate ->
-           // exchangeRatesRecyclerview
-        })
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        linearLayoutManager = LinearLayoutManager(context)
+        exchangeRatesRecyclerview.layoutManager = linearLayoutManager
+        exchangeRatesAdapter = ExchangeRatesAdapter()
+        exchangeRatesRecyclerview.adapter = exchangeRatesAdapter
+
+        setupBindings()
 
         addExchangeRatesButton.setOnClickListener {
             openNewExchangeRateDialog()
         }
     }
 
-    //region Utils
+    private fun setupBindings() {
+        viewModel?.exchangeRates?.observe(viewLifecycleOwner, Observer { exchangeRates ->
+            exchangeRatesAdapter.updateExchangeRates(exchangeRates)
+        })
+    }
+
+    //region
 
     private fun openNewExchangeRateDialog() = if (viewModel != null) {
         val addExchangeRateFragment = NewExchangeRateFragment(
